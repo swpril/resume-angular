dbc7e97cdad46864d497f7c6cbb5b5becf3bdc45
Swpril Ahuja <ahuja.s0007@gmail.com>
2021-03-11 21:18:00 +0530

    Setup forms and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'resume';
+export class AppComponent implements OnInit {
+  personalInfoFormGroup: FormGroup;
+  experienceInfoFormGroup: FormGroup;
+  projectsFormGroup: FormGroup;
+  educationFormGroup: FormGroup;
+  miscFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.personalInfoFormGroup = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: [
+        null,
+        Validators.compose([Validators.required, Validators.email])
+      ],
+      phone: [null, Validators.required],
+      linkedin: [null, Validators.required],
+      github: [null, Validators.required],
+      skills: [null, Validators.required]
+    });
+
+    this.experienceInfoFormGroup = this.formBuilder.group({
+      exp1_org: [null, Validators.required],
+      exp1_dur: [null, Validators.required],
+      exp1_desc: [null, Validators.required],
+      exp2_org: [null, Validators.required],
+      exp2_dur: [null, Validators.required],
+      exp2_desc: [null, Validators.required]
+    });
+
+    this.projectsFormGroup = this.formBuilder.group({
+      proj1_title: [null, Validators.required],
+      proj1_link: [null, Validators.required],
+      proj1_desc: [null, Validators.required],
+      proj2_title: [null, Validators.required],
+      proj2_link: [null, Validators.required],
+      proj2_desc: [null, Validators.required]
+    });
+
+    this.educationFormGroup = this.formBuilder.group({
+      edu1_school: [null, Validators.required],
+      edu1_year: [null, Validators.required],
+      edu1_qualification: [null, Validators.required],
+      edu1_desc: [null, Validators.required],
+      edu2_school: [null, Validators.required],
+      edu2_year: [null, Validators.required],
+      edu2_qualification: [null, Validators.required],
+      edu2_desc: [null, Validators.required]
+    });
+
+    this.miscFormGroup = this.formBuilder.group({
+      extra_1: [null, Validators.required],
+      extra_2: [null, Validators.required],
+      extra_3: [null, Validators.required],
+      extra_4: [null, Validators.required],
+      extra_5: [null, Validators.required]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
